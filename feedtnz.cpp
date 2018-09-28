27db1a1449c62fdbabbdd0f2fd762b87103f957b
Guillaume VARA <guillaume.vara@gmail.com>
2018-09-28 09:43:11 +0200

    du mieux ?

--- feedtnz.cpp
+++ feedtnz.cpp
@@ -1,40 +1,13 @@
- // $Id: //poco/1.4/Foundation/samples/Timer/src/Timer.cpp#1 $
- // This sample demonstrates the Timer and Stopwatch classes.
- // Copyright (c) 2004-2006, Applied Informatics Software Engineering GmbH.
- // and Contributors.
- // SPDX-License-Identifier:    BSL-1.0
+// $Id: //poco/1.4/Foundation/samples/Timer/src/Timer.cpp#1 $
+// This sample demonstrates the Timer and Stopwatch classes.
+// Copyright (c) 2004-2006, Applied Informatics Software Engineering GmbH.
+// and Contributors.
+// SPDX-License-Identifier:    BSL-1.0
 
- #include "Poco/Timer.h"
- #include "Poco/Thread.h"
- #include "Poco/Stopwatch.h"
- #include <iostream>
- #include "QtWidgets/QApplication"
+#include <iostream>
+#include <QtWidgets/qapplication.h>
 
- using Poco::Timer;
- using Poco::TimerCallback;
- using Poco::Thread;
- using Poco::Stopwatch;
-
- class TimerExample{
- public:
-     TimerExample(){ _sw.start();}
-
-     void onTimer(Timer& timer){
-         std::cout << "Callback called after " << _sw.elapsed()/1000 << " milliseconds." << std::endl;
-     }
- private:
-     Stopwatch _sw;
- };
-
- int main(int argc, char** argv){
-    
-     TimerExample example;
-     Timer timer(250, 500);
-     timer.start(TimerCallback<TimerExample>(example, &TimerExample::onTimer));
-
-     Thread::sleep(5000);
-     timer.stop();
-
-    QApplication app = new QApplication(argc, argv); 
+int main(int argc, char** argv){
+    QApplication app(argc, argv); 
     return app.exec();
- }
+}
