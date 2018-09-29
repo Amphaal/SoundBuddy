f3169ed8b01aad3bd954187318cc77d666572d52
Guillaume VARA <guillaume.vara@gmail.com>
2018-09-29 03:20:12 +0200

    Enculé de ta race

--- feedtnz.cpp
+++ feedtnz.cpp
@@ -1,11 +1,4 @@
-// $Id: //poco/1.4/Foundation/samples/Timer/src/Timer.cpp#1 $
-// This sample demonstrates the Timer and Stopwatch classes.
-// Copyright (c) 2004-2006, Applied Informatics Software Engineering GmbH.
-// and Contributors.
-// SPDX-License-Identifier:    BSL-1.0
-
-#include <iostream>
-#include <QtWidgets/qapplication.h>
+#include <QApplication>
 
 int main(int argc, char** argv){
     QApplication app(argc, argv); 
