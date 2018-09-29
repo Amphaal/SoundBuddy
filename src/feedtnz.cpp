8039e3bf2c47d0d0802017726976b369391b2c76
Guillaume VARA <guillaume.vara@gmail.com>
2018-09-29 17:44:25 +0200

    going forward

--- src/feedtnz.cpp
+++ src/feedtnz.cpp
@@ -1,9 +1,17 @@
 #include <QApplication>
+#include <QIcon>
 #include <QPushButton>
 
 int main(int argc, char** argv){
-    QApplication app(argc, argv); 
+    QApplication app(argc, argv);
+    
+    //Add app icon for windows
+    QIcon mainIcon(":/src/resources/icon.ico");
+    app.setWindowIcon(mainIcon);
+
+    //inst
     QPushButton bouton("Salut les Zéros, la forme ?");
     bouton.show();
+    
     return app.exec();
 }
