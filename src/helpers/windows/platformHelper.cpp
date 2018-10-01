68fd5b4d0a44417f5b13dd9ec01a92e750fb9036
Guillaume VARA <guillaume.vara@gmail.com>
2018-10-02 00:26:38 +0200

    tem

--- src/helpers/windows/platformHelper.cpp
+++ src/helpers/windows/platformHelper.cpp
@@ -1,22 +1,32 @@
 #include <windows.h>
-
+#include  <stdlib.h>  
 #include <string>
 #include "../../libs/plistcpp/Plist.hpp"
 
 using namespace std;
 
 class PlatformHelper {
-    public:
+    
+    static void openFileInOS(wchar_t* cpURL) {
+        ShellExecute(NULL, L"open", cpURL, NULL, NULL, SW_SHOWNORMAL);
+    }
 
-        static void openFileInOS(wchar_t* cpURL) {
-            ShellExecute(NULL, L"open", cpURL, NULL, NULL, SW_SHOWNORMAL);
+    static string getITunesPrefFileProbableLocation() {
+        return PlatformHelper::getenv("APPDATA") + string("\\Apple Computer\\Preferences\\com.apple.iTunes.plist");
+    }
+
+    static string getenv(char* variable) {
+        char* buf = nullptr;
+        size_t sz = 0;
+        if (_dupenv_s(&buf, &sz, "EnvVarName") == 0 && buf != nullptr)
+        {
+            string ret = variable;
+            free(buf);
+            return ret;
         }
+    }
 
-        static string getITunesPrefFileProbableLocation() {
-            return getenv("APPDATA") + string("\Apple Computer\Preferences\com.apple.iTunes.plist");
-        }
+    static string getProbableITunesLibraryLocation(std::ifstream *fileStream) {
 
-        static string getProbableITunesLibraryLocation(std::ifstream *fileStream) {
-
-        } 
+    } 
 }
